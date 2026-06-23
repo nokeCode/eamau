68de9972ebb421e2e85e5fdd0974882c9e54cc7d
nokeCode <nokegermain@gmail.com>
2026-06-23 08:41:19 +0000

    feat: ajout de l'annimation à feature carousel

--- lib/widgets/featured_news_carousel.dart
+++ lib/widgets/featured_news_carousel.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'feature_news_card.dart';
 import 'featured_news_card.dart';
+import 'dart:async';
 
 class FeaturedNewsCarousel extends StatefulWidget {
   const FeaturedNewsCarousel({super.key});
@@ -12,7 +13,40 @@ class FeaturedNewsCarousel extends StatefulWidget {
 class _FeaturedNewsCarouselState extends State<FeaturedNewsCarousel> {
   final PageController _controller = PageController(viewportFraction: 0.92);
 
+  late Timer _timer;
+
+  final int totalPages = 3;
+
   int currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _timer = Timer.periodic(
+      const Duration(seconds: 4),
+          (_) {
+        if (_controller.hasClients) {
+          currentPage++;
+
+          _controller.animateToPage(
+            currentPage,
+            duration: const Duration(
+              milliseconds: 500,
+            ),
+            curve: Curves.easeInOut,
+          );
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,20 +54,25 @@ class _FeaturedNewsCarouselState extends State<FeaturedNewsCarousel> {
       children: [
         SizedBox(
           height: 240,
-          child: PageView(
+          child: PageView.builder(
             controller: _controller,
             onPageChanged: (index) {
               setState(() {
                 currentPage = index;
               });
             },
-            children: const [
-              FeaturedNewsCard(image: "assets/images/actualite1.jpg"),
+            itemBuilder: (BuildContext context, int index) {
+              final images = [
+                "assets/images/actualite1.jpg",
+                "assets/images/actualite2.jpg",
+                "assets/images/actualite3.jpg",
+              ];
 
-              FeaturedNewsCard(image: "assets/images/actualite2.jpg"),
-
-              FeaturedNewsCard(image: "assets/images/actualite3.jpg"),
-            ],
+              return FeaturedNewsCard(
+                image: images[
+                index % images.length],
+              );
+            },
           ),
         ),
 
@@ -42,19 +81,30 @@ class _FeaturedNewsCarouselState extends State<FeaturedNewsCarousel> {
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: List.generate(
-            3,
-            (index) => AnimatedContainer(
-              duration: const Duration(milliseconds: 300),
-              margin: const EdgeInsets.symmetric(horizontal: 4),
-              width: currentPage == index ? 24 : 8,
-              height: 8,
-              decoration: BoxDecoration(
-                color: currentPage == index
-                    ? const Color(0xFF0A84FF)
-                    : Colors.grey.shade300,
-                borderRadius: BorderRadius.circular(20),
-              ),
-            ),
+            totalPages,
+                (index) {
+              final selected =
+                  currentPage % totalPages ==
+                      index;
+
+              return AnimatedContainer(
+                duration:
+                const Duration(milliseconds: 300),
+                margin:
+                const EdgeInsets.symmetric(
+                  horizontal: 4,
+                ),
+                width: selected ? 24 : 8,
+                height: 8,
+                decoration: BoxDecoration(
+                  color: selected
+                      ? const Color(0xFF0A84FF)
+                      : Colors.grey.shade300,
+                  borderRadius:
+                  BorderRadius.circular(20),
+                ),
+              );
+            },
           ),
         ),
       ],
