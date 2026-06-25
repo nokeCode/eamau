85af4e7580a8322c7f7976edd385c4c52168c0b2
nokeCode <nokegermain@gmail.com>
2026-06-25 09:31:40 +0000

    config: ajout du package carousel

--- lib/widgets/home/admission_banner.dart
+++ lib/widgets/home/admission_banner.dart
@@ -1,141 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class AdmissionBanner extends StatelessWidget {
   const AdmissionBanner({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 260,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
+
+    final List<String> images = [
+      'assets/images/building1.jpg',
+      'assets/images/building2.jpg',
+      'assets/images/building3.jpg',
+    ];
+
+    return CarouselSlider.builder(
+      itemCount: images.length,
+
+      options: CarouselOptions(
+        height: 250,
+
+        viewportFraction: 1,
+
+        autoPlay: true,
+
+        autoPlayInterval: const Duration(seconds: 4),
+
+        autoPlayAnimationDuration:
+        const Duration(milliseconds: 800),
+
+        enlargeCenterPage: false,
+
+        enableInfiniteScroll: true,
       ),
-      child: Stack(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Image.asset(
-              'assets/images/building.jpg',
-              width: double.infinity,
-              height: double.infinity,
-              fit: BoxFit.cover,
-            ),
-          ),
 
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              gradient: LinearGradient(
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
-                colors: [
-                  Color(0xFF173B7A),
-                  Color(0xAA173B7A),
-                  Colors.transparent,
-                ],
-              ),
-            ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 10,
-                    vertical: 5,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.blue.shade300,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: const Text(
-                    'ADMISSIONS 2025-2026',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 10,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-
-                const SizedBox(height: 15),
-
-                const Text(
-                  'Construisez\nvotre avenir\navec EAMAU',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 26,
-                    fontWeight: FontWeight.bold,
-                    height: 1.1,
-                  ),
-                ),
-
-                const SizedBox(height: 10),
-
-                const Text(
-                  'Excellence académique,\nleadership de demain.',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 12,
-                  ),
-                ),
-
-                const Spacer(),
-
-                ElevatedButton(
-                  onPressed: () {},
-
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.black,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
-
-                  child: const Text(
-                    'En savoir plus',
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-          const Positioned(
-            bottom: 12,
-            left: 0,
-            right: 0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-
-                CircleAvatar(
-                  radius: 3,
-                  backgroundColor: Colors.white,
-                ),
-
-                SizedBox(width: 5),
-
-                CircleAvatar(
-                  radius: 3,
-                  backgroundColor: Colors.white54,
-                ),
-
-                SizedBox(width: 5),
-
-                CircleAvatar(
-                  radius: 3,
-                  backgroundColor: Colors.white54,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+      itemBuilder: (
+          context,
+          index,
+          realIndex,
+          ) {
+        return _BannerItem(
+          imagePath: images[index],
+        );
+      },
     );
   }
 }
